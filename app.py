7130801c9332162edde6e8be8d77cfe06279bad1
alvsgabriellk <cauagabriell81@gmail.com>
2025-11-19 11:37:01 -0300

    Rota de login passada para o app do flask

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
-from routes.user_routes import user_bp
+from routes.user_routes import user_bp, login_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(user_bp, url_prefix="/users")
+app.register_blueprint(login_bp, url_prefix="/api")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
