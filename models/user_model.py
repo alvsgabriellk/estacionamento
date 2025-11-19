28d51690c92c17e6540eb7727569e1cba40700a8
alvsgabriellk <cauagabriell81@gmail.com>
2025-11-19 10:38:04 -0300

    Validação de matricula criada.

--- models/user_model.py
+++ models/user_model.py
@@ -1,15 +1,29 @@
 from crud.database.connection import get_connection
 
-def cadastro_usuario(nome, sobrenome, email, senha_hash, cpf, matricula):
+
+def validar_matricula(codigo):
+    conex = get_connection()
+    cursor = conex.cursor()
+
+    sql = "SELECT id FROM matriculas_validas where codigo = %s"
+    cursor.execute(sql, (codigo,))
+    existe = cursor.fetchone()
+
+    cursor.close()
+    conex.close()
+
+    return existe is not None
+
+def cadastro_usuario(nome, sobrenome, email, senha_hash, cpf, matricula1, matricula2=None):
     conex = get_connection()
     cursor = conex.cursor()
 
     sql = """
-    INSERT INTO usuario (nome, sobrenome, email, senha_hash, cpf, matricula)
-    VALUES (%s, %s, %s, %s, %s, %s)
+    INSERT INTO usuario (nome, sobrenome, email, senha_hash, cpf, matricula1, matricula2)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
     """
 
-    cursor.execute(sql, (nome, sobrenome, email, senha_hash, cpf, matricula))
+    cursor.execute(sql, (nome, sobrenome, email, senha_hash, cpf, matricula1, matricula2))
     conex.commit()
     cursor.close()
     conex.close()
