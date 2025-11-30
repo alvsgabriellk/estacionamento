c72e245cd0695d1158a0ebd75e5b065ba7f8dc4e
marcela-leite <marcelaleite.uni@gmail.com>
2025-11-30 11:29:24 -0300

    ajuste final <3

--- modelos/modelo-grafico.js
+++
@@ -1,9 +0,0 @@
-export interface Order {
-  id: number;
-  clientName: string;
-  value: number;
-  cnpj: string;
-  status: 'Em agendamento' | 'Em produção' | 'Em processo de entrega' | 'Entregue';
-  dateMade: Date;
-  deliveryDate: Date;
-}
